1e93fb04a53e46c0cfe0e703f791624edef5f053
Thắng Lê <lequocthang307@gmail.com>
2023-09-24 15:06:51 +0700

    sb

---
+++ 07_spring_data_repository/excercise/blog/src/main/java/com/blog/service/BlogService.java
@@ -0,0 +1,45 @@
+package com.blog.service;
+
+import com.blog.model.Blog;
+import com.blog.repository.IBlogRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class BlogService implements IBlogService{
+    @Autowired
+    private IBlogRepository blogRepository;
+    @Override
+    public void createBlog(Blog blog) {
+        blogRepository.save(blog);
+    }
+
+//    @Override
+//    public List<Blog> showAll() {
+//        return blogRepository.findAll();
+//    }
+
+    @Override
+    public void editBlog(Blog blog) {
+        blogRepository.save(blog);
+    }
+
+    @Override
+    public void deleteBlog(int id) {
+
+        blogRepository.deleteById(id);
+    }
+
+    @Override
+    public Page<Blog> search(String name, Pageable pageable) {
+        return blogRepository.searchByOwn("%" + name + "%", pageable);
+    }
+
+    @Override
+    public Blog findById(int id) {
+        return blogRepository.findById(id).get();
+    }
+}
